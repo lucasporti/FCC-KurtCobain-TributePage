99bfea3aa20239afc6661706da049532851617af
lupor <lucassportillo09@gmail.com>
2023-07-28 12:51:10 -0300

    Buton scroll to top created

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const btnToTop = document.querySelector("#btnScrollToTop");
+
+btnToTop.addEventListener("click", () =>{
+    document.documentElement.scrollTop = 0;
+})
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY < 500) btnToTop.style.right = -100 + "px";
+    else btnToTop.style.right = 5 + "px";
+})
